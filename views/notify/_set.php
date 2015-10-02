f7e3d16b15b128f289a164ef8171ecd8744d5195
Bruce Aldridge <bruce@incode.co.nz>
2015-10-02 21:01:57 +1300

    remove notify from sets with no rrp

--- views/notify/_set.php
+++ views/notify/_set.php
@@ -13,33 +13,37 @@ use yii\helpers\Html;
 
 ?>
 <div>
-    <?php if ($notifySuccess) {
-        ?><p>Thanks, we will let you know if the price goes under <?=$notifySet->percent_off?>%</p><?php
-    } else { ?>
-        <?php $form = ActiveForm::begin([
-                'id'     => 'notify',
-                'enableClientValidation' => false,
-            ]
-        ); ?>
-        <?php echo $form->errorSummary([$notifyAddress, $notifySet]); ?>
-        <div class="row">
-            <div class="col-md-6">
-                <?= $form->field($notifyAddress, 'email')->textInput(['maxlength' => 90])->label('Email me') ?>
+    <?php if (!$model->rrp) { ?>
+        <p>Sorry, notifications cannot be sent for this product as no RRP is set</p>
+    <?php } else { ?>
+        <?php if ($notifySuccess) {
+            ?><p>Thanks, we will let you know if the price goes under <?=$notifySet->percent_off?>%</p><?php
+        } else { ?>
+            <?php $form = ActiveForm::begin([
+                    'id'     => 'notify',
+                    'enableClientValidation' => false,
+                ]
+            ); ?>
+            <?php echo $form->errorSummary([$notifyAddress, $notifySet]); ?>
+            <div class="row">
+                <div class="col-md-6">
+                    <?= $form->field($notifyAddress, 'email')->textInput(['maxlength' => 90])->label('Email me') ?>
 
+                </div>
+                <div class="col-md-6">
+
+                    <?= $form->field($notifySet, 'percent_off', [
+                        'inputTemplate' => '<div class="input-group">{input}<span class="input-group-addon">%</span></div>',
+                    ])->label('if discount is at least') ?>
+                </div>
             </div>
-            <div class="col-md-6">
+            <?= Html::activeHiddenInput($notifySet, 'set_code') ?>
 
-                <?= $form->field($notifySet, 'percent_off', [
-                    'inputTemplate' => '<div class="input-group">{input}<span class="input-group-addon">%</span></div>',
-                ])->label('if discount is at least') ?>
-            </div>
-        </div>
-        <?= Html::activeHiddenInput($notifySet, 'set_code') ?>
-
-        <?= Html::submitButton('' .  Yii::t('app', 'Notify Me'), [
-            'id' => 'save-' . $model->formName(),
-            'class' => 'btn btn-primary btn-sm'
-        ]); ?>
-        <?php $form->end(); ?>
+            <?= Html::submitButton('' .  Yii::t('app', 'Notify Me'), [
+                'id' => 'save-' . $model->formName(),
+                'class' => 'btn btn-primary btn-sm'
+            ]); ?>
+            <?php $form->end(); ?>
+        <?php } ?>
     <?php } ?>
 </div>
